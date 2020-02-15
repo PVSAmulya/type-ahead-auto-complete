54e6e9efaf1dfdb609de58a88f79fbee048ea733
AmulyaPVS <amulya@elpaviation.com>
2020-02-14 21:31:42 -0600

    added initial routing, initial components

---
+++ src/app/modules/main/main-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main.component';
+
+const routes: Routes = [
+  {
+    path: '', component: MainComponent,
+    children: [
+      {
+        path: '', loadChildren: () => import('../auto-complete/auto-complete.module').then(mod => mod.AutoCompleteModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MainRoutingModule { }
